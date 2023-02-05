652c586b42030007ab584515a25af087844d4c06
luoys <luoys@guahao.com>
2023-02-05 14:19:10 +0800

    v3

--- src/main/java/com/luoys/upgrade/toolservice/schedule/TaskSchedule.java
+++ src/main/java/com/luoys/upgrade/toolservice/schedule/TaskSchedule.java
@@ -3,6 +3,7 @@ package com.luoys.upgrade.toolservice.schedule;
 import com.luoys.upgrade.toolservice.service.SuiteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
